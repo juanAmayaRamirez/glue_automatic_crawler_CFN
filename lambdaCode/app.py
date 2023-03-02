45aeff1f2c96bcf72f2a73e6e3afe8cd154f18e2
Juan Felipe Amaya Ramirez <amaya_ramirez97@outlook.com>
2023-03-02 17:18:13 -0500

    Added Automatic Glue Workflow
    * lambda event trigger
    * added glue triggers for glue workflow

--- lambdaCode/app.py
+++ lambdaCode/app.py
@@ -8,11 +8,9 @@ logger.setLevel(logging.INFO)
 
 # Variables
 REGION = 'us-east-1'
-CRAWLER=os.environ['GLUE_CRAWLER']
+TRIGGER=os.environ['TRIGGER']
 
-# Import Boto 3 for AWS Glue
-import boto3
-from botocore.exceptions import ClientError
+# from botocore.exceptions import ClientError
 glue = boto3.client(service_name='glue', region_name=REGION)
 
 
@@ -20,10 +18,13 @@ glue = boto3.client(service_name='glue', region_name=REGION)
 def lambda_handler(event, context):
     try:
         logger.info('## TRIGGERED BY S3 PUT EVENT')
-        logger.info(f'## STARTING GLUE CRAWLER: {CRAWLER}')
-        glue.start_crawler(Name=CRAWLER)
-    except ClientError as e:
-        if e.response.get('Error', {}).get('Code') == 'CrawlerRunningException':
-            logger.info(f'## GLUE CRAWLER: {CRAWLER} ALREADY RUNNING')
-        else:
-            raise e
+        logger.info(f'## STARTING GLUE TRIGGER: {TRIGGER}')
+        response = glue.start_trigger(Name=TRIGGER)
+        return response
+    except Exception as e:
+        raise e
+        # if e.response.get('Error', {}).get('Code') == 'CrawlerRunningException':
+        #     logger.info(f'## GLUE TRIGGER: {TRIGGER} ALREADY RUNNING')
+        # else:
+        #     raise e
+    
